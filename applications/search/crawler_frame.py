12d73638905376f79c2d4eda2e98153417320c8c
Shirby Wang <shirby.wang@gmail.com>
2017-05-01 13:46:37 -0700

    added IDs

--- applications/search/crawler_frame.py
+++ applications/search/crawler_frame.py
@@ -28,10 +28,10 @@ class CrawlerFrame(IApplication):
     def __init__(self, frame):
         self.starttime = time()
         # Set app_id <student_id1>_<student_id2>...
-        self.app_id = ""
+        self.app_id = "13353358_34811375_90269441"
         # Set user agent string to IR W17 UnderGrad <student_id1>, <student_id2> ...
         # If Graduate studetn, change the UnderGrad part to Grad.
-        self.UserAgentString = None
+        self.UserAgentString = "IR S17 UnderGrad 13353358, 34811375, 90269441"
 		
         self.frame = frame
         assert(self.UserAgentString != None)
